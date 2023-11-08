49b93c10614dffcd72aab3b02f2864f60aad1d24
whalini <150257195+whalini@users.noreply.github.com>
2023-11-08 21:26:10 +0800

    add files

---
+++ contracts/PDATimeLock.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+library TransferHelper {
+    function safeApprove(address token, address to, uint value) internal {
+        // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: APPROVE_FAILED');
+    }
+
+    function safeTransfer(address token, address to, uint value) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FAILED');
+    }
+
+    function safeTransferFrom(address token, address from, address to, uint value) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
+    }
+
+    function safeTransferETH(address to, uint value) internal {
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+    }
+}
+
+contract PDATimeLock {
+  address public owner;
+  uint public end;
+  constructor(address _takeToken, uint _duration) {
+    owner = _takeToken;
+    end = block.timestamp + _duration;
+  }
+
+  function takeOutToken(
+      address _token,
+      uint256 _amount,
+      address _to
+  ) external {
+      require(msg.sender == owner, "not auth");
+      require(block.timestamp > end, "too early");
+      if (_token == address(0)) {
+          TransferHelper.safeTransferETH(_to, _amount);
+      } else {
+          TransferHelper.safeTransfer(_token, _to, _amount);
+      }
+  }
+}
